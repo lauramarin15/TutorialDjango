89bf601b6ee6fabc969018251e192c8f572df38b
Laura <lmarinvelez0@gmail.com>
2026-02-04 21:28:34 -0500

    applicatioh running

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse#new
+from django.views.generic import TemplateView#new
 # Create your views here.
+
+def homePageView(request):#new
+    return HttpResponse("Hello, World!")#new
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
