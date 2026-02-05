126f40ddb8f9790b7aff9228f6c12ce1902e8a6a
Laura <lmarinvelez0@gmail.com>
2026-02-04 23:02:30 -0500

    acividad 3

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homePageView  #new
+from .views import homePageView, AboutPageView,ProductShowView, ProductIndexView, ContactPageView #new
 
 
 urlpatterns = [
     path('', homePageView, name='home'),  #new
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('contacto/', ContactPageView.as_view(), name='contacto'),
 ]
 
  
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 #from django.http import HttpResponse#new
 from django.views.generic import TemplateView#new
 # Create your views here.
@@ -12,3 +13,58 @@ from django.views.generic import TemplateView#new
 
 def homePageView(request):
     return render(request, "pages/home.html")
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Laura Marín",
+    })
+        return context
+    
+class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV"},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+    ]
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+        return render(request, self.template_name, viewData)
+    
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id)-1]
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
+
+class ContactPageView(View):
+
+    template_name = 'pages/contacto.html'
+
+    def get(self, request):
+
+        context = {
+            "nombre": "Laura",
+            "email": "lmarinv2@eafit.edu.co",
+            "phone": "1234567890",
+        }
+
+        return render(request, self.template_name, context)
