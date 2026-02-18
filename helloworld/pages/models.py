4c90b77a2f0b595f01473a864d7a044fe4575450
Laura <lmarinvelez0@gmail.com>
2026-02-18 09:01:32 -0500

    Part b

--- helloworld/pages/models.py
+++ helloworld/pages/models.py
@@ -14,3 +14,12 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    price = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
